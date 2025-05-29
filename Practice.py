e7a7a187f5c3c3d415b8448e34b61bfe75655f73
joysd1010Edu <joysd.1005@fec.edu.bd>
2025-05-29 12:09:42 +0600

    refined logic and input validation

--- Practice.py
+++ Practice.py
@@ -10,21 +10,22 @@ reversedLibrary={
     1:"scissors"
 }
 while True:
-    player=input("Enter rock, paper, or scissors: ").lower()
+    player=input("Enter 'exit' to exit\nEnter rock, paper, or scissors: ").lower()
+    if player=='exit':
+        print("Exiting the game.")
+        break
+    elif player not in library:
+        print("Invalid input. Please enter rock, paper, or scissors.")
+        continue
    
     print(f"computer chose {reversedLibrary[computer]} and you chose {reversedLibrary[library[player]]}.")
     if library[player] == computer:
         print("It's a tie!")
-    elif library[player]==computer+1 or (library[player]==computer+2):
+    elif library[player] == computer + 1 or (library[player] == computer - 2):
         print("You win!")
-    elif library[player]==computer-1 or (library[player]==computer-2):
-        print("You lose!")
-    elif player=='exit':
-        print("Exiting the game.")
-        break
     else:
-        print("Invalid input. Please enter rock, paper, or scissors.")
-        continue
+        print("You lose!")
+
 
 
 
